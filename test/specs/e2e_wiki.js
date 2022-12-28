8a599389ac18eafb26fcd651c0d5680a4410e07d
nick.kontsevych <nick.kontsevych@gmail.com>
2022-12-28 12:57:58 +0200

    Code refactoring

--- test/specs/e2e_wiki.js
+++ test/specs/e2e_wiki.js
@@ -1,6 +1,7 @@
 
 import MainPage from '../pageobjects/main.page.js'
 import mainPage from "../pageobjects/main.page.js";
+import WikiApple from "../pageobjects/wiki.apple.js";
 
 describe('Wiki main page', () => {
     it('Verify wiki main page elements', async ()  => {
@@ -11,5 +12,11 @@ describe('Wiki main page', () => {
         await expect(MainPage.btnSearch).toBeExisting();
     });
 })
-
-
+describe('Search result ', () => {
+    it('Verify wiki main page elements', async ()  => {
+        await MainPage.open();
+        await MainPage.fieldSearch.addValue('Apple')
+        await MainPage.btnSearch.click();
+        await expect(WikiApple.pageTitleApple).toBeExisting()
+    });
+})
